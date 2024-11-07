ed5340aee5934cccf9c69069515f16fedbc13816
LE THANH DAT <lethanhdathd2908@gmail.com>
2024-11-07 23:58:30 +0700

    prefixSum

---
+++ PrefixSum.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+const int MAX = 100005;
+int n;
+int a[MAX];
+int f[MAX];
+int q;
+
+using namespace std;
+
+void input(){
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+}
+
+void prefixSum(){
+    f[0] = a[0];
+    for (int i = 1; i < n; i++){
+        f[i] = f[i-1] + a[i];
+    }
+}
+
+void solve(){
+    prefixSum();
+    cin >> q;
+    for (int i = 0; i < n; i++){
+        int x, y;
+        cin >> x >> y;
+        cout << f[y - 1] - f[x - 2] << "\n";
+    }
+}
+
+int main(){
+    input();
+    solve();
+    return 0;
+}
