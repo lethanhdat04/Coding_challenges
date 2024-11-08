84f40c6733aff7cd20226e2ff0095720ef105d10
LE THANH DAT <lethanhdathd2908@gmail.com>
2024-11-08 23:56:06 +0700

    simulatorStack

---
+++ PrefixSum2D.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1005;
+int n,m,q;
+int a[MAX][MAX];
+int f[MAX][MAX];
+int r1,c1,r2,c2;
+
+void input(){
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin >> a[i][j];
+        }
+    }
+}
+
+void prefixSum(){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            f[i][j] = f[i-1][j] + f[i][j-1] - f[i-1][j-1] + a[i][j];
+        }
+    }
+}
+
+void solve(){
+    prefixSum();
+    cin >> q;
+    for (int i = 0; i < q; i++){
+        cin >> r1 >> c1 >> r2 >> c2;
+        if (i < q - 1){
+            cout << f[r2-1][c2-1] - f[r1-1-1][c2-1] - f[r2-1][c1-1-1] + f[r1-1-1][c1-1-1] << "\n";
+        } else {
+            cout << f[r2-1][c2-1] - f[r1-1-1][c2-1] - f[r2-1][c1-1-1] + f[r1-1-1][c1-1-1];
+        }
+    }
+}
+
+int main(){
+    input();
+    solve();
+    return 0;
+}
---
+++ SimulatorStack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include<stack>
+
+using namespace std;
+
+int main(){
+    string command;
+    stack<int> st;
+    int value;
+    while (cin >> command && command != "#"){
+        if (command == "PUSH"){
+            cin >> value;
+            st.push(value);
+            cout << value << " ";
+        } else if (command == "POP"){
+            if (!st.empty()){
+                cout << st.top() << endl;
+                st.pop();
+            } else {
+                cout << "NULL" << endl;
+            }
+        }
+    }
+    // while (!st.empty()){
+    //     cout << st.top() << "\n";
+    //     st.pop();
+    // }
+    return 0; 
+}
