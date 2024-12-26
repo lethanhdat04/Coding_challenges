b851d47ab291b3317cbeb419b86efa62674b8e97
thanhdat2908 <datprince123@gmail.com>
2024-12-26 21:00:30 +0700

    Update CBus.cpp

--- CBus.cpp
+++ CBus.cpp
@@ -2,151 +2,59 @@
 
 using namespace std;
 
-#define MAX 100
+const int maxN = 12;
+int n, k, load = 0, res = 1e9, sum;
+int c[maxN*2][maxN*2];
+int appear[maxN*2] = {0};
+int d[2*maxN][2*maxN];
+int x[2*maxN];
 
-
-int N;// number of requests (1,2,...,N). Request i has pickup point i and drop-off point i + N
-
-int cap;// number of places of the bus
-
-int A[2*MAX+1][2*MAX+1];
-
-int x[MAX];
-
-int appear[MAX];// marking
-
-int load;
-
-int f;
-
-int f_best;
-
-int x_best[MAX];
-
-int cmin;
-
-void input(){
-
-    scanf("%d%d",&N,&cap);
-
-    cmin = 1000000;
-
-    for(int i = 0; i <= 2*N;  i++){
-
-        for(int j= 0; j <= 2*N; j++){
-
-            scanf("%d",&A[i][j]);
-
-            if(i != j && cmin > A[i][j]) cmin = A[i][j];
-
+void inp() {
+    cin >> n >> k;
+    for (int i = 0; i <= 2*n; i++) {
+        for (int j = 0; j <= 2*n; j++) {
+            cin >> c[i][j];
         }
-
     }
-
-
-
+    x[0] = 0;
 }
 
-
-
-int check(int v, int k){
-
-    if(appear[v] == 1) return 0;
-
-    if(v >N){
-
-        if(!appear[v-N]) return 0;
-
-    }else{
-
-        if(load + 1 > cap) return 0;
-
+int check(int i) {
+    if (appear[i]) return 0;
+    if (i > n) {
+        if (!appear[i - n]) return 0;
     }
-
+    if (i < n && load == k) return 0;
     return 1;
-
 }
 
-void solution(){
-
-    if(f + A[x[2*N]][0] < f_best){
-
-        f_best = f + A[x[2*N]][0];
-
-        for(int i = 0; i <= 2*N; i++) x_best[i] = x[i];
-
-        //printf("update best %d\n",f_best);
-
-    }
-
-}
-
-void TRY(int k){
-
-    for(int v = 1; v <=2*N; v++){
-
-        if(check(v,k)){
-
-            x[k] = v;
-
-            f += A[x[k-1]][x[k]];
-
-            if(v <= N) load += 1; else load += -1;
-
-            appear[v] = 1;
-
-            if(k == 2*N) solution();
-
-            else{
-
-                if(f + (2*N+1-k)*cmin < f_best)
-
-                    TRY(k+1);
-
-            }
-
-            if(v <= N) load -= 1; else load -= -1;
-
-            appear[v] = 0;
-
-            f -= A[x[k-1]][x[k]];
-
+void TRY(int k) {
+    for (int i = 1; i <= 2*n; i++) {
+        if (check(i)) {
+            if (i > n) 
+                load -= 1;
+            else
+                load += 1;
+            x[k] = i;
+            appear[i] = 1;
+            sum += c[x[k-1]][i];
+            if (k == 2*n) {
+                res = min(res, sum + c[x[k]][0]);
+            } else
+                TRY(k+1);
+            if (i > n) 
+                load += 1;
+            else
+                load -= 1;
+            sum -= c[x[k-1]][i];
+            appear[i] = 0;
         }
-
     }
-
 }
 
-void solve(){
-
-    load = 0;
-
-    f = 0;
-
-    f_best = 1000000;
-
-    for(int i = 1; i <= 2*N; i++) appear[i] = 0;
-
-    x[0] = 0;// starting point
-
+int main() {
+    inp();
     TRY(1);
-
-    printf("%d",f_best);
-
-}
-
-void print(){
-
-    for(int i = 0; i <= 2*N; i++) printf("%d ",x_best[i]);
-
-}
-
-int main(){
-
-    input();
-
-    solve();
-
-  return 0;
-
+    cout << res << endl;
+    return 0;
 }
